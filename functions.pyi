1038e0b09afdb120520713c8bafff62c4964f5ab
marksikaundi <emmanuelmark117@gmail.com>
2022-03-16 09:40:31 +0200

    Added: function

---
+++ functions.pyi
@@ -0,0 +1,25 @@
+# math functions
+import math
+
+# ratio = signal_power / noise_power
+# decibels = 10 * math.log10(ratio)
+
+# math.radians = 0.7
+# height = math.sin(math.radians)
+
+# examples 1
+
+# note that the execution can only take place using the python ide
+degrees = 45
+radians = degrees / 180.0 * math.pi
+math.sin(radians)
+
+# output 
+# 0.7071067811865476
+
+math.sqrt(2) / 2.0
+
+# OutPut
+# 0.7071067811865476
+
+
